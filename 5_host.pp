9d64c9f728e0f8a7a6629faef810c0ec187d0079
jainkunal25 <34001748+jainkunal25@users.noreply.github.com>
2019-06-04 13:10:56 +0200

    Delete 5_host.pp

--- 5_host.pp
+++
@@ -1,6 +0,0 @@
-host { 'localhost':
-  ensure       => 'present',
-  host_aliases => ['localhost.localdomain', 'localhost4', 'localhost4.localdomain4'],
-  ip           => '127.0.0.1',
-  target       => '/etc/hosts',
-}
